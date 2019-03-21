316bdc97fba719e7b43374fe18ea4cc92381d602
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-03-21 15:38:20 -0400

    Clearing room for DB connection

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Redis;
 |
 */
 
+/*
 Route::get('videos/{id}', function ($id) {
     $downloads = Redis::get("videos.{$id}.downloads");
     return view('welcome')->withDownloads($downloads);
@@ -25,3 +26,4 @@ Route::get('videos/{id}/download', function ($id){
 
     return back();
 });
+*/
