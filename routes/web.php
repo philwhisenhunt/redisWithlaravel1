d10902edf5817febe71f5395e67f2468fd838c05
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-03-27 12:41:43 -0400

    Cleaned up

--- routes/web.php
+++ routes/web.php
@@ -119,12 +119,9 @@ Route::get('foo', function(){
 // });
 
 Route::get('weather/{coordinates}', function($coordinates){
-    // include(app_path() . '\Functions\weatherFunction.php');
-   
-    // return view('coordWeather');
-    // $coordinates = '39.0997,94.5786';
+  
     $answer = weatherFunction($coordinates);
-    // return $answer;
+   
 
     return view('coordWeather', ['answer' => $answer]);
 });     
